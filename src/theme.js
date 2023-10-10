254268d562c8172811544a92125a936b88fa7a08
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2023-10-11 07:47:16 +0900

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    // palette: {
+    //     primary: {
+    //         main: "#1760a5",
+    //         light: "skyblue"
+    //     },
+    //     secondary: {
+    //         main: '#15c630',
+    //     },
+    //     otherColor: {
+    //         main: "#999"
+    //     }
+    // }
+    myButton: {
+        backgroundColor: "red",
+        color: "white",
+        border: "1px solid black"
+    }
+})
